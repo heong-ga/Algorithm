eb6d4b5e4b37757844fb19eaa85ae152ba1f8e36
heong.ga <heong.ga@gmail.com>
2019-09-29 21:29:46 +0900

    2747번 피보나치수

---
+++ DP/2747.py
@@ -0,0 +1,13 @@
+memo = [0] * 46
+
+def fib(n) :
+    if n <= 1:
+        return n
+    elif memo[n] != 0 :
+        return memo[n]
+    else :
+        memo[n] = fib(n-1) + fib(n-2)
+        return memo[n]
+
+n = int(input())
+print(fib(n))
